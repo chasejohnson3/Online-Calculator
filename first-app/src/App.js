75684f4aab9eea037c065a86004da9b0ef31a258
Chase Johnson <chaser.johnson27@gmail.com>
2018-01-24 19:27:32 -0600

    Brought in the files I started in class but want to continue to work on.

---
+++ first-app/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Display">0</div>
+        <div className="Keyboard">
+          <button>C</button>
+          <button>+/-</button>
+          <button>%</button>
+          <button>/</button>
+
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>X</button>
+
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button></button>
+
+          <button></button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+
+          
+
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div> 
+               
+      </div>
+    );
+  }
+}
+
+export default App;
