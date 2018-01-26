c0bbf00fc9aab4c3d04b1a5f4c5e8eb0b299ddca
Chase Johnson <chaser.johnson27@gmail.com>
2018-01-25 18:05:38 -0600

    Making components for NumberButton's

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,39 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+class Display extends Component{
+  render(){
+    return <div className="Display">0</div>
+  }
+}
+
+class Keyboard extends Component{
+render(){
+  return (
+    <div className="Keyboard">
+      <button>C</button>
+      <button>+/-</button>
+      <button>%</button>
+      <button class="button button_orange">/</button>
+
+      {/* <button>7</button> */}
+      <NumberButton number={7}/>
+      <button>8</button>
+      <button>9</button>
+      <button class="button button_orange">X</button>
+
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button class="button button_orange">-</button>
+
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button class="button button_orange">+</button>             
+
+      <button class="button button_zero">0</button>
+      <button>.</button>
+      <button class="button button_orange">=</button>
+    </div> 
+  )
+}
+}
+
+class NumberButton extends Component{
+  render(){
+    return <button> {this.props.number} </button>
+  }
+}
+
+
+
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <div className="Display">0</div>
-        <div className="Keyboard">
-          <button>C</button>
-          <button>+/-</button>
-          <button>%</button>
-          <button>/</button>
-
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>X</button>
-
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button></button>
-
-          <button></button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-
-          
-
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-        </div> 
+        <Display />
+        <Keyboard />
                
       </div>
     );
@@ -41,3 +64,31 @@ class App extends Component {
 }
 
 export default App;
+
+// <div className="Display">0</div>
+// <div className="Keyboard">
+//   <button>C</button>
+//   <button>+/-</button>
+//   <button>%</button>
+//   <button class="button button_orange">/</button>
+
+//   <button>7</button>
+//   <button>8</button>
+//   <button>9</button>
+//   <button class="button button_orange">X</button>
+
+//   <button>4</button>
+//   <button>5</button>
+//   <button>6</button>
+//   <button class="button button_orange">-</button>
+
+//   <button>1</button>
+//   <button>2</button>
+//   <button>3</button>
+//   <button class="button button_orange">+</button>             
+
+//   <button>0</button>
+//   <button>0</button>
+//   <button>.</button>
+//   <button class="button button_orange">=</button>
+// </div> 
